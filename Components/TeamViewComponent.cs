c67a1528f616f4326954c493b5d088a0c74c6044
jrmoore808 <61597022+jrmoore808@users.noreply.github.com>
2021-03-31 09:47:35 -0600

    Add project files.

---
+++ Components/TeamViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//ViewCompnent for Teams. Teams' Default.cshtml can be found in Views/Shared/Components/Team
+namespace BowlingLeague.Components
+{
+    public class TeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public TeamViewComponent (BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedTeam = RouteData?.Values["team"];
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using BowlingLeague.Models;
+using BowlingLeague.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BowlingLeagueContext context;
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext ctx)
+        {
+            _logger = logger;
+            context = ctx;
+        }
+
+        //Index page. Organizes list of bowlers and sets up pagination for them
+        public IActionResult Index(long? teamid, string team, int pagenum = 0)
+        {
+            int pagesize = 5;
+
+            return View(new IndexViewModel
+            {
+                Bowlers = context.Bowlers
+                    .Where(x => x.TeamId == teamid || teamid == null)
+                    .OrderBy(x => x.BowlerLastName)
+                    .Skip((pagenum - 1) * pagesize)
+                    .Take(pagesize)
+                    .ToList(),
+
+                PageNumInfo = new PageNumInfo
+                {
+                    NumItemsPerPage = pagesize,
+                    CurrentPage = pagenum,
+                    TotalNumItems = (teamid == null ? context.Bowlers.Count() : context.Bowlers.Where(x => x.TeamId == teamid).Count())
+                },
+
+                TeamCategory = team
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
